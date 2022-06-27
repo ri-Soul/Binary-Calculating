bb491026e3daa8cfd1704315ec47ebf2a13aa3f0
ERR403 <91799709+ri-Soul@users.noreply.github.com>
2022-06-27 21:37:43 +0200

    Update Bin-Dec.py

--- Bin-Dec.py
+++ Bin-Dec.py
@@ -1,11 +1,11 @@
 from math import *
-Binary = input("Enter the Binary value: ")
-i = len(Binary)
+binary = input("Enter the Binary: ")
+i = len(binary)
 decimal = 0
+print("Calculating...")
 
-for letter in Binary:
+for letter in binary:
   i -= 1
   decimal += (float(letter)* (pow(2, i)))
-  print(decimal)
 
 print("The decimal: " + str(decimal))
